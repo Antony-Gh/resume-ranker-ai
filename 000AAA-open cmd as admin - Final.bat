e159a5b1c4c1403876cd8457011c2a37241e585a
Antony Ghayes <121578807+Antony-Gh@users.noreply.github.com>
2025-04-17 22:59:07 +0200

    Delete 000AAA-open cmd as admin - Final.bat

--- 000AAA-open cmd as admin - Final.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Get the current directory
-set current_dir=%cd%
-
-:: Open CMD as admin in the current directory
-powershell -Command "Start-Process cmd -ArgumentList '/K cd /d %current_dir%' -Verb RunAs"
-
-:: Pause to see the output (optional)
-:: pause
