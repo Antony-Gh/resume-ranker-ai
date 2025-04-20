b19b84c5e4edd0377de74c0396d6756c49309950
Antony Ghayes <121578807+Antony-Gh@users.noreply.github.com>
2025-04-20 04:30:07 +0200

    Update RedirectIfAuthenticatedAndVerified.php

--- app/Http/Middleware/RedirectIfAuthenticatedAndVerified.php
+++ app/Http/Middleware/RedirectIfAuthenticatedAndVerified.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,6 +16,8 @@ class RedirectIfAuthenticatedAndVerified
     public function handle(Request $request, Closure $next): Response
     {
         $user = Auth::user();
+
+        Log::info("User: " + json_encode($user));
 
         if ($user && $user->hasVerifiedEmail()) {
             return redirect()->route('dashboard');
